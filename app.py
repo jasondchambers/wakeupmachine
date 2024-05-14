96db8aaabb8d13bef23fb7ec657a608ce41912df
Jason Chambers <jason.d.chambers@gmail.com>
2024-05-14 16:36:29 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+#import argparse
+#from wakeonlan import send_magic_packet
+
+#def main():
+    #parser = argparse.ArgumentParser() 
+    #parser.add_argument('mac', help='MAC address') 
+    #args = parser.parse_args() 
+    #print(f'Sending magic wakeup packet to {args.mac}')
+    #send_magic_packet(args.mac)
+#
+#if __name__ == "__main__":
+    #main()
+
+
+#import os
+#from flask import Flask
+#from markupsafe import escape
+#
+#app = Flask(__name__)
+#
+#@app.route("/<mac>")
+#def home(mac):
+    #send_magic_packet(mac)
+    #return f"Waking up {escape(mac)}"
+#
+#if __name__ == "__main__":
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(debug=True, host='0.0.0.0', port=port)
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return "Hello World!"
+
