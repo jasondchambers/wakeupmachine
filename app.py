f82aa329291022892dea9d4cb3ecf5938b79274b
Jason Chambers <jason.d.chambers@gmail.com>
2024-05-15 12:32:07 +0000

    Fix

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from wakeonlan import send_magic_packet
 from markupsafe import escape
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/wakeup/<mac>")
 def wakeup(mac):
-    print(mac)
     send_magic_packet(mac)
-    return render_template('index.html', mac=escape(mac))
-    #return f"Waking up {escape(mac)}"
+    return render_template('wakeup.html', mac=escape(mac))
 
